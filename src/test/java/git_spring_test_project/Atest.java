aa833ecb258f9e5d0b174b3d565d82a77214536a
luguiming <luguiming@chexiang.com>
2014-10-06 14:30:19 +0800

    1

---
+++ src/test/java/git_spring_test_project/Atest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C), 2013-2014, 上汽集团
+ * FileName: Atest.java
+ * Author:   luguiming
+ * Date:     2014年10月6日 下午2:29:44
+ * Description: //模块目的、功能描述      
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+package git_spring_test_project;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈功能详细描述〉
+ *
+ * @author luguiming
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+public class Atest {
+
+    /**
+     * 功能描述: <br>
+     * 〈功能详细描述〉
+     *
+     * @param args
+     * @see [相关类/方法](可选)
+     * @since [产品/模块版本](可选)
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
